9d2424a30967a6c6bb123386be6c2f42e2e7bc94
Charles Joma <almprincecharles@yahoo.com>
2025-07-23 17:23:45 +1000

    Webpack :: Adjust URL points

--- client/src/api/helpers.js
+++ client/src/api/helpers.js
@@ -2,7 +2,7 @@ import api from "utils/utils.js";
 
 export const getAllMenuItems = async () => {
   try {
-    const response = await api.post("/api/catalog", {});
+    const response = await api.post("/catalog", {});
     return response.data;
   } catch (error) {
     console.error(error);
@@ -11,7 +11,7 @@ export const getAllMenuItems = async () => {
 
 export const getItemById = async (id) => {
   try {
-    const response = await api.get(`/api/catalog/${id}`);
+    const response = await api.get(`/catalog/${id}`);
     return response.data;
   } catch (error) {
     console.error(error);
